969eb741c0aff3f7faf5819022deecaa1d34f16e
Cong Vu Chi <nogogold1998@gmail.com>
2021-01-29 15:03:52 +0700

    create xml and view

--- app/src/main/java/com/example/assignment/gradle/ui/MainActivity.kt
+++ app/src/main/java/com/example/assignment/gradle/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.assignment.gradle
+package com.example.assignment.gradle.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.assignment.gradle.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
